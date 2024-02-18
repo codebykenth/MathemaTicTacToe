815ab9a8a15eeb258888fe78e7b8086c976bad33
kenth <kenth@LAPTOP-832N4QBD>
2024-02-19 00:31:30 +0800

    1st commit

---
+++ src/Class/Main.java
@@ -0,0 +1,14 @@
+package Class;
+
+import Frames.*;
+
+/**
+ *
+ * @author Kenth Alexis Osila
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainJFrame mainJFrame = new MainJFrame();
+        mainJFrame.setVisible(true);
+    }
+}
---
+++ src/Interface/CheckTileTurn.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interface;
+
+/**
+ *
+ * @author Kenth Alexis Osila
+ */
+public interface CheckTileTurn {
+
+    void checkTile1Turn();
+
+    void checkTile2Turn();
+
+    void checkTile3Turn();
+
+    void checkTile4Turn();
+
+    void checkTile5Turn();
+
+    void checkTile6Turn();
+
+    void checkTile7Turn();
+
+    void checkTile8Turn();
+
+    void checkTile9Turn();
+
+}
